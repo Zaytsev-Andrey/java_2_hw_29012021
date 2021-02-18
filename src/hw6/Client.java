e6f06e82b66f14c4c6de2cc9b0c8158de10100dc
Zaytsev-Andrey <hendrey@yandex.ru>
2021-02-18 20:43:10 +0300

    added homework6

--- src/hw6/Client.java
+++ src/hw6/Client.java
@@ -1,7 +1,81 @@
 package hw6;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Client {
+    private static final String IP_ADDRESS = "localhost";
+    private static final int PORT = 8098;
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(IP_ADDRESS, PORT)) {
+            System.out.println("[Client started]");
+
+            try (DataInputStream in = new DataInputStream(socket.getInputStream());
+                 DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+
+                Thread listenerService = new Thread(() -> {
+                   String msg;
+
+                    // Пока клиент не завершит работу читаем сообщения от сервера
+                    while (!Thread.currentThread().isInterrupted()) {
+                       try {
+                           msg = in.readUTF();
+
+                           // Если сервер прислал "команду" завершения сеанса, тогда выходим из цикла
+                           if (msg.equals("/end")) {
+                               System.out.println("[The server closed the connection]");
+                               break;
+                           }
+
+                           System.out.printf("[%s] %s\n", dateFormat.format(new Date()), msg);
+                       } catch (SocketException e) {
+                           System.out.println("[The input stream stopped]");
+                       } catch (IOException e) {
+                           e.printStackTrace();
+                       }
+                   }
+                });
+                listenerService.start();
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                    String msg;
+
+                    // Пока поток чтения сообщений от сервера не завершен, читаем введенные сообщения с консоли
+                    while (listenerService.isAlive()) {
+
+                        // Ждем пока пользователь введет сообщение
+                        if (reader.ready()) {
+                            msg = reader.readLine();
+
+                            // Если между чтением с консоли и отправкой сообщения серверу, сервер не завершил
+                            // сеанс - отправляем введенное сообщение
+                            // Иначе выходим из цикла
+                            if (listenerService.isAlive()) {
+                                out.writeUTF(msg);
+                                out.flush();
+
+                                // Если пользователь ввел "команду" завершения сеанса, тогда останавливаем поток
+                                // чтения сообщений от сервера и выходим из цикла
+                                if (msg.equals("/end")) {
+                                    listenerService.interrupt();
+                                    System.out.println("[The connection is closing...]");
+                                    break;
+                                }
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/hw6/Server.java
+++ src/hw6/Server.java
@@ -1,7 +1,84 @@
 package hw6;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Server {
+    private static final int PORT = 8098;
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("[Server started]");
+
+            try (Socket socket = server.accept()) {
+                System.out.println("[Client connected]");
+
+                try (DataInputStream in = new DataInputStream(socket.getInputStream());
+                     DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+
+                    Thread listenerService = new Thread(() -> {
+                        String msg;
+
+                        // Пока сервер не завершит работу читаем сообщения от клиента
+                        while (!Thread.currentThread().isInterrupted()) {
+                            try {
+                                msg = in.readUTF();
+
+                                // Если клиент прислал "команду" завершения сеанса, тогда выходим из цикла
+                                if (msg.equals("/end")) {
+                                    System.out.println("[The client has completed the connection]");
+                                    break;
+                                }
+
+                                System.out.printf("[%s] %s\n", dateFormat.format(new Date()), msg);
+                            } catch (SocketException e) {
+                                System.out.println("[The input stream stopped]");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    });
+                    listenerService.start();
+
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+                        String msg;
+
+                        // Пока поток чтения сообщений от клиента не завершен, читаем введенные сообщения с консоли
+                        while (listenerService.isAlive()) {
+
+                            // Ждем пока пользователь введет сообщение
+                            if (reader.ready()) {
+                                msg = reader.readLine();
+                                // Если между чтением с консоли и отправкой сообщения клиенту, клиент не завершил
+                                // сеанс - отправляем введенное сообщение
+                                // Иначе выходим из цикла
+                                if (listenerService.isAlive()) {
+                                    out.writeUTF(msg);
+                                    out.flush();
+
+                                    // Если пользователь ввел "команду" завершения сеанса, тогда останавливаем поток
+                                    // чтения сообщений от клиента и выходим из цикла
+                                    if (msg.equals("/end")) {
+                                        System.out.println("[The connection is closing...]");
+                                        listenerService.interrupt();
+                                        break;
+                                    }
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
